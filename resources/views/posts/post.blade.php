cc259da95c87cac2029fc7f236f852f3de38f92b
edriso <edriso.dev@gmail.com>
2023-05-14 19:28:32 +0300

    fix css path

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -12,11 +12,10 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="/assets/css/app.css">
 </head>
 
 <body>
-    {{-- {{ $post }} --}}
     <?php echo $post; ?>
 
     <a href="/">Go Back</a>
--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="/assets/css/app.css">
 </head>
 
 <body>
