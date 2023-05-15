360bbe19d338eb4a48ee2b98422cb8f10cf73d12
edriso <edriso.dev@gmail.com>
2023-05-15 17:42:22 +0300

    using blade directives

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -16,9 +16,9 @@
 </head>
 
 <body>
-    <article>
+    <article class="post">
         <h2>{{ $post->title }}</h2>
-        <?= $post->body ?>
+        {!! $post->body !!}
     </article>
 
     <a href="/">Go Back</a>
--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -16,12 +16,14 @@
 </head>
 
 <body>
-    @foreach ($posts as $post)
-        <article>
-            <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
-            <p>{{ $post->excerpt }}</p>
-        </article>
-    @endforeach
+    <main class="posts-wrapper">
+        @foreach ($posts as $post)
+            <article class="{{ $loop->even ? 'post post--even' : 'post' }}">
+                <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+                <p>{{ $post->excerpt }}</p>
+            </article>
+        @endforeach
+    </main>
 </body>
 
 </html>
